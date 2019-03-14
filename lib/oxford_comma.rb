9d7dc89db2a7b37d789b31b0ba1aa1108e51b5f5
Leila Weiss <leila.weiss@powerhrg.com>
2019-03-14 16:26:53 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array}"
-
-array.join
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else array.length == 3
+    array[0..-2].join(", ") + ", and " + array[-1]
   end
-
-
-
+end
